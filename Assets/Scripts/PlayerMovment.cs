920175e302fa81c842cae79db87cbe16a8387c7c
CyberBow <rizksalimrizk@gmail.com>
2023-06-23 15:24:19 +0300

    add animations

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 public class PlayerMovment : MonoBehaviour
 {
     public Rigidbody rb;
+    public Animator animator;
+
     public float forwardspeed = 1000f;
     public float sidespeed = 100f;
 
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
+
+    void UpdateAnimation()
+    {
+        animator.SetBool("SprintLeft", isMovingLeft);
+        animator.SetBool("SprintRight", isMovingRight);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
-    {
+    {  
         rb.AddForce(0, 0, forwardspeed * Time.deltaTime);
 
         if (Input.GetKey("d"))
         {
             rb.AddForce(sidespeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            isMovingRight = true;
+            isMovingLeft = false;
         }
-        if (Input.GetKey("a"))
+        else if (Input.GetKey("a"))
         {
             rb.AddForce(-sidespeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            isMovingLeft = true;
+            isMovingRight = false;
         }
+        else
+        {
+            isMovingLeft = false;
+            isMovingRight = false;
+        }
+
+        UpdateAnimation();
     }
 }
